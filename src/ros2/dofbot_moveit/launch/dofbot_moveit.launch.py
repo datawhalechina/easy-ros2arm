90280350986a62218d25468354e44c129ac5f42f
liwanye <evan.wy.li@gmail.com>
2023-11-02 22:06:04 +0800

    adding content 20231102

--- src/ros2/dofbot_moveit/launch/dofbot_moveit.launch.py
+++ src/ros2/dofbot_moveit/launch/dofbot_moveit.launch.py
@@ -41,6 +41,12 @@ def generate_launch_description():
         condition=UnlessCondition(LaunchConfiguration('gui'))
     )
 
+    joint_state_publisher_gui_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        condition=IfCondition(LaunchConfiguration('gui'))
+    )
+
     rviz_node = Node(
         package='rviz2',
         executable='rviz2',
@@ -54,6 +60,7 @@ def generate_launch_description():
         model_arg,
         rviz_arg,
         joint_state_publisher_node,
+        joint_state_publisher_gui_node,
         robot_state_publisher_node,
         rviz_node
     ])

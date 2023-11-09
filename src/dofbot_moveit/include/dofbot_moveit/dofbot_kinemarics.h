1ca50efd622c7d432bdd00da474326ea358e55ea
liwanye <evan.wy.li@gmail.com>
2023-11-09 17:33:34 +0800

    modify dofbot_moveit 20231109

---
+++ src/dofbot_moveit/include/dofbot_moveit/dofbot_kinemarics.h
@@ -0,0 +1,36 @@
+#ifndef ARM_MOVEIT_ARM_KINEMATRICS_H
+#define ARM_MOVEIT_ARM_KINEMATRICS_H
+
+#include<vector>
+
+#include "rclcpp/rclcpp.hpp"
+#include "kdl/chain.hpp"
+#include "kdl/tree.hpp"
+#include "kdl/segment.hpp"
+#include "kdl/frames_io.hpp"
+#include "kdl/chainiksolverpos_lma.hpp"
+#include "kdl/chainfksolverpos_recursive.hpp"
+#include "kdl_parser/kdl_parser.hpp"
+
+class Dofbot{
+public:
+    /**
+     * Forward kinematics calculation Obtain the current pose according to the rotation joint angle
+     * 正向运动学计算 根据旋转关节角获取当前位姿
+     * @param urdf_file 模型文件路径  model file path
+     * @param joints    当前关节角度  Current joint angle
+     * @param cartpos   当前末端位姿  current end pose
+     */
+    bool dofbot_getFK(const char *urdf_file, std::vector<double> &joints, std::vector<double> &currentPos);
+    /**
+     * Inverse kinematics calculation Obtain the angle that each joint needs to rotate to reach the target point
+     * 逆运动学计算 获取到到目标点各关节需要转动的角度
+     * @param urdf_file  模型文件路径    model file path
+     * @param targetXYZ  目标位置       target location
+     * @param targetRPY  目标姿态       target pose
+     * @param outjoints  目标点关节角度  Target point joint angle
+     */
+    bool dofbot_getIK(const char *urdf_file, std::vector<double> &targetXYZ, std::vector<double> &targetRPY, std::vector<double> &outjoints);
+};
+
+#endif
